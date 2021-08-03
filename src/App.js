13a2ecea23037a59d44635ad6717929902d73694
Daniel Orchel <karma0rg4u@gmail.com>
2021-08-03 20:57:43 +0100

    Add delete option

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text:'asdasdasdasds asdsd',
+      day: 'August 1st at 1:30pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text:'asdsddasasa dfhfds asdsd',
+      day: 'August 3rd at 5:00am',
+      reminder: false
+    }
+  ])
+
+  const deleteTask = id => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="container">
-      <Header></Header>
+      <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        'No Tasks To Do'
+      )}
     </div>
   );
 }
